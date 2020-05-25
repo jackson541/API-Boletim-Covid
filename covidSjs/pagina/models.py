0fa85dd061ae0ac3a7547fa80d00032b9be381ba
JacksonAlves123 <jacksonalves541@gmail.com>
2020-05-25 20:46:47 -0300

    criacao do modelo de casos e alternativa para erro do deletar em massa

--- covidSjs/pagina/models.py
+++ covidSjs/pagina/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class casos (models.Model):
+    data = models.DateField(primary_key=True)
+    confirmados = models.IntegerField()
+    recuperados = models.IntegerField()
+    suspeitos = models.IntegerField()
+    descartados = models.IntegerField()
+    obitos = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = 'casos'
+
+    def __str__(self):
+        #converte do formato ano-mes-dia para dia/mes/ano e transforma em string
+        return self.data.strftime('%d/%m/%Y')
