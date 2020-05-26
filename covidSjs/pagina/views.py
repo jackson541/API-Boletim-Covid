baeb79abede462fe8e0d117a3ea2d29e875f99d6
JacksonAlves123 <jacksonalves541@gmail.com>
2020-05-25 21:02:44 -0300

    adicionando ultimo objeto do bd a index

--- covidSjs/pagina/views.py
+++ covidSjs/pagina/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+
+from .models import casos as Casos
 
 # Create your views here.
+
+class index(View):
+    def get(self, request):
+        casos = Casos.objects.latest('data')
+        return render(request, 'index.html', {'casos': casos})
