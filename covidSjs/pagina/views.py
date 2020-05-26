bc6cc95ad63b75cb9506178e9bab566744b5a3c4
JacksonAlves123 <jacksonalves541@gmail.com>
2020-05-26 10:11:21 -0300

    css dos cards e final da lógica desses

--- covidSjs/pagina/views.py
+++ covidSjs/pagina/views.py
@@ -7,5 +7,19 @@ from .models import casos as Casos
 
 class index(View):
     def get(self, request):
-        casos = Casos.objects.latest('data')
-        return render(request, 'index.html', {'casos': casos})
+        casoAtual = Casos.objects.latest('data')
+
+        # inverte a ordem da lista completa de objetos, que estão ordenados pela
+        # data, e pega o segundo da lista (penúltimo)
+        penultimoCaso = Casos.objects.all().order_by('data')[::-1][1]
+
+        casosNovos = {
+            'confirmados': casoAtual.confirmados - penultimoCaso.confirmados,
+            'recuperados': casoAtual.recuperados - penultimoCaso.recuperados,
+            'suspeitos': casoAtual.suspeitos - penultimoCaso.suspeitos,
+            'descartados': casoAtual.descartados - penultimoCaso.descartados,
+            'obitos': casoAtual.obitos - penultimoCaso.obitos
+        }
+
+        return render(request, 'index.html', {'casoAtual': casoAtual, 
+                                            'casosNovos': casosNovos})
