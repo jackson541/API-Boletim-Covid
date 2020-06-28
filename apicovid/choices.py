04e3743627ab720bb8cc825df0e9f22a53e84b90
JacksonAlves123 <jacksonalves541@gmail.com>
2020-06-28 20:19:43 -0300

    alter faixa_choices and create choices.py file

---
+++ apicovid/choices.py
@@ -0,0 +1,27 @@
+#arquivo para armazenar todas as choices utilizadas no models.py
+
+TIPO_CHOICES = [
+    ('confirmado', 'Confirmado'),
+    ('tratamento', 'Em tratamento'),
+    ('internado', 'Internado'),
+    ('recuperado', 'Recuperado'),
+    ('suspeito', 'Suspeito'),
+    ('obito', 'Óbito'),
+    ('descartado', 'Descartado'),
+    ('notificado', 'Notificado')
+]
+
+GENERO_CHOICES = [
+    ('feminino', 'Feminino'),
+    ('masculino', 'Masculino'),
+    ('outro', 'Outro')
+]
+
+FAIXA_CHOICES = [
+    ('0-19', '0-19'),
+    ('20-39', '20-39'),
+    ('40-59', '40-59'),
+    ('60-69', '60-69'),
+    ('70-79', '70-79'),
+    ('80+', '80+')
+]
