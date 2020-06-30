9358099e52b1357397a32d84cafc9d9d118bcb3f
JacksonAlves123 <jacksonalves541@gmail.com>
2020-06-29 21:25:33 -0300

    create token route and use token for test in CasosList

--- apicovid/views.py
+++ apicovid/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import permissions
+from rest_framework.authentication import TokenAuthentication
 
 from .models import *
 from .serializers import *
 
 
 class CasoList(generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    
     queryset = Caso.objects.all()
     serializer_class = CasoSerializer
 
