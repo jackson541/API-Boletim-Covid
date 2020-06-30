3b68e843ca48078543b39a252b335705b4969556
jonathantvrs <jonathantvrs@ufrn.edu.br>
2020-06-30 12:18:09 -0300

    adicionando na API o CRUD de Cidade

--- apicovid/serializers.py
+++ apicovid/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import *
+from .models import Caso, Cidade
+
+class CidadeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cidade
+        fields = ['id', 'nome', 'responsavel', 'numero_habitantes']
 
 class CasoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Caso
         fields = ['id', 'boletim', 'tipo', 'genero', 'faixa', 'quantidade']
+
