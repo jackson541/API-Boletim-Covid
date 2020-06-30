69bb17ff3a8fef327b6daad3e60e62236bed391e
JacksonAlves123 <jacksonalves541@gmail.com>
2020-06-30 13:42:20 -0300

    adiciona chave estrangeira em Usuario para Cidade

--- apicovid/models.py
+++ apicovid/models.py
@@ -15,4 +15,6 @@ class Caso(models.Model):
 class Usuario(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='usuario')
     criador = models.ForeignKey(User, on_delete=models.CASCADE, related_name='criador')
+    cidade = models.IntegerField()
+    #cidade = models.ForeignKey(Cidade, on_delete=models.CASCADE)
     
