665d24fc04cc37097034bd10be56d1a8ccec7b35
jonathantvrs <jonathantvrs@ufrn.edu.br>
2020-07-14 11:47:30 -0300

    adiciona postgresql como sgbd padrão

--- APIBoletimCovid/settings.py
+++ APIBoletimCovid/settings.py
@@ -79,6 +79,14 @@ WSGI_APPLICATION = 'APIBoletimCovid.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'apicovid',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
+    },
+    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
